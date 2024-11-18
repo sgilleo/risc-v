970ef7b78a3ed486ed5d5b36a3d0d5b01394e177
Alejandro Lopez Azorin <alopazo@upv.edu.es>
2024-11-18 13:46:23 +0100

    Cambios

--- hardware/ROM.sv
+++ hardware/ROM.sv
@@ -1,9 +1,9 @@
-module ROM (
-    input logic [9:0] address,
+module ROM #(parameter address_width=1024) (
+    input logic [($clog2(address_width)-1):0] address,
     output logic [31:0] instruccion
 );
 
-logic [31:0] memoria[1023:0];
+logic [31:0] memoria[(address_width-1):0];
 
 initial
 	$readmemh("ROM.hex",memoria);
