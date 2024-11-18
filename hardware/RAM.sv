12caaa7a686697977dbe6f96b41d074e7332f8a7
AbatsasTel <abatsas@upv.edu.es>
2024-11-18 12:57:38 +0100

    Update RAM.sv
    Como el tamaño de la direcciones deja de ser parametrizable

--- hardware/RAM.sv
+++ hardware/RAM.sv
@@ -1,13 +1,13 @@
 //Alejandro Bataller Sastre
-module RAM #(parameter address_width = 1024, data_width = 32)
+module RAM #(parameter address_width = 1024)
 (
 input CLK, MemWrite, //Memwrite senyal de habilitacion de escritura
-input [data_width-1:0] write_data,
+input [31:0] write_data,
 input[$clog2(address_width)-1:0] address, 
-output [data_width-1:0] read_data
+output [31:0] read_data
 );
 
-logic [data_width-1:0] memoria [address_width-1:0];
+logic [31:0] memoria [address_width-1:0];
 
 
 always_ff @(posedge CLK)
