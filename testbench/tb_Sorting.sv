79c749a9de7620c5a48080ff1d3d49b7087aa6ae
sgilleo <sergiogilleon2004@gmail.com>
2024-12-09 19:07:45 +0100

    Añadido testbench para algoritmo de ordenado

---
+++ testbench/tb_Sorting.sv
@@ -0,0 +1,65 @@
+`timescale 1ns/1ns
+
+module tb_Fibonacci();
+
+    logic CLK, RSTn;
+
+    logic MemWrite, MemRead;
+    logic [9:0] address_DMEM, address_IMEM;
+    logic [31:0] Instruction, data_DMEM, write_data_DMEM;
+
+    RAM ram(
+        .CLK(CLK),
+        .MemWrite(MemWrite),
+        .MemRead(MemRead),
+        .write_data(write_data_DMEM),
+        .address(address_DMEM),
+        .read_data(data_DMEM)
+    );
+
+    ROM #(.file("D:\\UNIVERSIDAD\\3TELECO\\ISDIGI\\risc-v\\assembly\\sorting.hex")) rom(
+        .address(address_IMEM),
+        .instruction(Instruction)
+    );
+
+    CPU_Core DUV(
+        .CLK(CLK), 
+        .RSTn(RSTn),
+        .Instruction(Instruction),
+        .data_DMEM(data_DMEM),
+        .address_IMEM(address_IMEM),
+        .address_DMEM(address_DMEM),
+        .write_data_DMEM(write_data_DMEM),
+        .MemWrite(MemWrite),
+        .MemRead(MemRead)
+    );
+
+    localparam T = 20;
+	
+	always
+	begin
+		#(T/2) CLK = ~CLK;
+	end
+
+    task reset;
+		@(negedge CLK);
+		RSTn = 1'b0;
+		@(negedge CLK);
+		RSTn = 1'b1;
+		@(negedge CLK); //1 ciclo de espera
+	endtask
+
+    initial begin
+        CLK = 1'b0;
+        RSTn = 1'b1;
+
+        reset();
+
+        repeat(3000) @(negedge CLK);
+        
+        $stop();
+
+    end
+
+
+endmodule
