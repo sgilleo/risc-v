0fba91a9cb74f8486c9fc7881c2b98a2c293d9a2
sgilleo <sergiogilleon2004@gmail.com>
2024-12-09 19:03:54 +0100

    Cambiado el programa de ordenacion

--- assembly/selection_sort.asm
+++ assembly/selection_sort.asm
@@ -1,45 +1,64 @@
-.text
-	la a0, numbers #Guardar la dirección de los datos
-    add x1, x0, x0 #Indice del bucle externo i en multiplos de 4
-    add x2, x0, x0 #Indice del bucle interno j en multiplos de 4
-    lw x3, n #Numero de bytes para ordenar
-    addi x4, x3, -4 #Numero de bytes para ordenar -4
+init: #Inicializar los valores en la memoria
+	addi x1, x0, 0x000 #Cargar la direccion de memoria inicial (Indice i)
     
+    addi x2, x0, 123
+    sw x2, 0(x1)
+	
+    addi x2, x0, 42
+    sw x2, 4(x1)
+    
+    addi x2, x0, 12
+    sw x2, 8(x1)
+    
+    addi x2, x0, 87
+    sw x2, 12(x1)
+    
+    addi x2, x0, 32
+    sw x2, 16(x1)
+    
+    addi x2, x0, 3
+    sw x2, 20(x1)
+    
+    addi x2, x0, 64
+    sw x2, 24(x1)
+    
+	addi x2, x0, 0 #Fin de los datos
+    sw x2, 28(x1)
+    
+
+
+
+#x1: Direccion i
+#x2: Direccion j
+#x3: Direccion dato minimo
+#x4: Valor dato actual (i)
+#x5: Valor dato en direccion j
+#x6: Valor dato en direccion minima
     
 loop:
-	bge x1, x4, end
-	add t0, x1, x0 #Indice del dato minimo
+	lw x4, 0(x1) #Valor del dato actual
+    beq x4, x0, end #Condicion de salida del algoritmo (ultimo dato es cero)
     
-    addi x2, x1, 4 #Comienzo del bucle interior en j = i+1
-inner_loop:
-	bge x2, x3, inner_loop_end #Condicion de salida del bucle interior
-	add a1, a0, x2 #Calculamos la direccion para el dato con indice j
-    lw t1, 0(a1) #Leemos el dato de indice j
-    
-    add a2, a0, t0 #Calculamos la direccion para el dato con indice minimo
-    lw t2, 0(a2) #Leemos el dato de indice minimo
-    
-	bge t1, t2, end_if
-if:
-	add t0, x2, x0 #Actualizamos el indice mínimo al indice j
-end_if:
-    addi x2, x2, 4 #Incrementamos j por 4 bytes 
-    j inner_loop
-inner_loop_end:
+    add x3, x0, x1 #Direccion del valor minimo
 	
-	#Intercambio del dato minimo con el actual:
-    add a1, a0, x1 #Calculamos la direccion para el dato con indice i
-    lw t1, 0(a1) #Leemos el dato de indice i
-    add a2, a0, t0 #Calculamos la direccion para el dato con indice minimo
-    lw t2, 0(a2) #Leemos el dato de indice minimo
-    sw t2, 0(a1) #Escribimos el nuevo numero menor en la posicion actual
-    sw t1, 0(a2) #Escribimos el numero de la posicion actual en la posicion del menor numero
-        
-    addi x1, x1, 4 
-    j loop
-end:
+    addi x2, x1, 4 #Comienzo del bucle interior en j = i+1 
+inner_loop:
+	lw x6, 0(x3) #Valor del dato minimo
     
-.data
-n: .word 56 #Cantidad de números a ordenar en bytes (4*N)
-numbers:
-	.word 45, 87, 32, 12, 98, 123, 5, 25, 37, 3, 72, 7, 14, 68
+	lw x5, 0(x2) #Valor del dato en la direccion j
+    beq x5, x0, inner_loop_end #Condicion de salida del bucle interior
+      
+	bge x5, x6, end_if
+if:
+	add x3, x0, x2 #Direccion minima igual a j
+end_if:
+	addi x2, x2, 4
+    beq x0, x0, inner_loop
+inner_loop_end:
+
+	sw x6, 0(x1) #Intercambiar valores
+    sw x4, 0(x3)
+
+	addi x1, x1, 4
+    beq x0, x0, loop
+end:
